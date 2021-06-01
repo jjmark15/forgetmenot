c8a97b73d668ef2c05a34a5f3795df161d3c9252
Josh Jones <ohblonddev@gmail.com>
2021-06-01 13:07:24 +0100

    Use parent process shell

--- src/ports/command_execution/system_process/system_process_command_executor_adapter.rs
+++ src/ports/command_execution/system_process/system_process_command_executor_adapter.rs
@@ -15,7 +15,8 @@ impl CommandExecutor for SystemProcessCommandExecutorAdapter {
         let spawn_result = if cfg!(target_os = "windows") {
             Command::new("cmd").args(&["/C", command.string()]).spawn()
         } else {
-            Command::new("sh").arg("-c").arg(command.string()).spawn()
+            let shell: String = std::env::var("SHELL").unwrap_or_else(|_| "sh".to_string());
+            Command::new(shell).arg("-c").arg(command.string()).spawn()
         };
 
         let result = spawn_result.and_then(|mut child| child.wait());
